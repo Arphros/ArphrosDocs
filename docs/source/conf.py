365b419093d55c217696fe62610b52d8d81d1075
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2021-12-11 16:34:32 +0700

    Markdown

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,6 +17,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    'myst_parser'
 ]
 
 intersphinx_mapping = {
@@ -26,7 +27,6 @@ intersphinx_mapping = {
 intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
-
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
